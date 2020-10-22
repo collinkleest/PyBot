ebfedf1926cb553de2c55adb85d3d28305705fe5
Collin Kleest <collinkleest@gmail.com>
2020-10-22 14:21:30 -0400

    update .gitignore to ignore .vscode/ directory add bot to app

--- bot.py
+++ bot.py
@@ -1,16 +1,26 @@
 import os
 
 import discord
+from discord.ext import commands
 import dotenv
 
 dotenv.load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+BOT_PREFIX = '/'
+
 intents = discord.Intents.default()
 intents.members = True
 
 client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix=BOT_PREFIX)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as: {bot.user.name}')
 
 
 @client.event
@@ -22,19 +32,8 @@ async def on_ready():
 
     print(
         f'{client.user} is connected to the following guild:\n'
-        f'{bot_guild.name}(id: {bot_guild.id}'
+        f'{bot_guild.name}(id: {bot_guild.id})'
     )
-
-    for channel in bot_guild.channels:
-        print(f'Channel Name: {channel.name}')
-
-    for member in bot_guild.members:
-        if member.name == "Collin Kleest":
-            i = 0
-            while (i < 100):
-                await member.create_dm()
-                await member.dm_channel.send("Sup lil bitch, your lucky they rate limit the discord api ")
-                i += 1
 
 
 @client.event
